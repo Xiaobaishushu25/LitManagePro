d79b767c723ec85a6ab8d2f816b1259b84119b75
Pang <2055761346@qq.com>
2025-03-22 20:00:43 +0800

    develop!!!!!!!

--- src-tauri/src/services/util/file.rs
+++ src-tauri/src/services/util/file.rs
@@ -23,6 +23,7 @@ async fn segment_pdf(path: &str, id: i32) -> AppResult<String> {
     }
     let tmp_dir = format!("{}/data/tmp", CURRENT_DIR.clone());
     fs::create_dir_all(tmp_dir.clone())?;
+    clear_tmp_dir(&tmp_dir)?;
     let out_path = format!("{}/output{}.pdf", tmp_dir, id);
     doc.save(out_path.clone())?;
     // println!("前 3 页已成功保留，其他页已删除。输出文件为 output.pdf");
@@ -47,6 +48,28 @@ pub fn get_and_save_icon(path: &str, size: u16) -> AppResult<(String, String)> {
         .save_with_format(save_path.clone(), image::ImageFormat::Png)
         .map_err(|e| Tip(format!("保存图标失败{:#}", e)))?;
     Ok((name.into(), save_path))
+}
+fn clear_tmp_dir(tmp_dir: &str) -> std::io::Result<()> {
+    // 检查目录是否存在
+    if !Path::new(tmp_dir).exists() {
+        return Ok(()); // 如果目录不存在，直接返回成功
+    }
+    // 获取目录中的所有条目
+    let entries = fs::read_dir(tmp_dir)?;
+    // 遍历每个条目并删除
+    for entry in entries {
+        let entry = entry?;
+        let path = entry.path();
+        // 如果是文件，直接删除
+        if path.is_file() {
+            fs::remove_file(&path)?;
+        }
+        // 如果是目录，递归删除目录及其内容
+        else if path.is_dir() {
+            fs::remove_dir_all(&path)?;
+        }
+    }
+    Ok(())
 }
 #[cfg(test)]
 mod test{
